c50259984c453a9ecd81ab7d0e300ae2a37498eb
unknown <unknown@example.com>
2022-09-13 18:09:31 +0800

    Initial Commit

---
+++ pages/home/home.js
@@ -0,0 +1,120 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // TODO
+    swiperData:[
+      {
+        id: 1,
+        src: "https://static.monknow.com/newtab/wallpaper/329360cd18afacd25c4192ee56d1ac27.jpg",
+        link: ""
+      },
+      {
+        id: 2,
+        src: "https://static.monknow.com/newtab/wallpaper/87f38c8890d25a721a9681873fd4c25f.jpg",
+        link: ""
+      }
+    ],
+    gridData:[
+      {
+        id: 1,
+        name : "test1",
+        icon: "/img/双子座.png"
+      },
+      {
+        id: 2,
+        name : "test1",
+        icon: "/img/双子座.png"
+      },
+      {
+        id: 3,
+        name : "test1",
+        icon: "/img/双子座.png"
+      },
+      {
+        id: 4,
+        name : "test1",
+        icon: "/img/双子座.png"
+      },
+      {
+        id: 5,
+        name : "test1",
+        icon: "/img/双子座.png"
+      },
+      {
+        id: 6,
+        name : "test1",
+        icon: "/img/双子座.png"
+      },
+      {
+        id: 7,
+        name : "test1",
+        icon: "/img/双子座.png"
+      },
+      {
+        id: 8,
+        name : "test1",
+        icon: "/img/双子座.png"
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
